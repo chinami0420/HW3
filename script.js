6efb474d7d060142d69b071951e192191b8f7906
Chinami Iihama <chinami0420unno@yahoo.co.jp>
2020-01-22 14:13:09 -0800

    revise js

--- script.js
+++ script.js
@@ -1,76 +1,80 @@
-var lowercase = "abcdefghijklmnopqrstuvwxyz",
-  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers = "0123456789",
-  punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
-  lowercaseInput = document.getElementById("lowercase"),
-  uppercaseInput = document.getElementById("uppercase"),
-  punctuationInput = document.getElementById("punctuation"),
-  numbersInput = document.getElementById("numbers"),
-  lengthInput = document.getElementById("length"),
-  passwordFeild = document.getElementById("pass-field"),
-  generateButton = document.getElementById("generate"),
-  copyButton = document.getElementById("copy"),
-  plength,
-  userPassword,
-  passwordCharSet;
+//
 
-function generate() {
-  userPassword = "";
-  passwordCharSet = "";
-  if (lowercaseInput.checked) {
-    passwordCharSet += lowercase;
-  }
-  if (uppercaseInput.checked) {
-    passwordCharSet += uppercase;
-  }
-  if (punctuationInput.checked) {
-    passwordCharSet += punctuation;
-  }
-  if (numbersInput.checked) {
-    passwordCharSet += numbers;
-  }
-  plength = Number(lengthInput.value);
+const resultEl = document.getElementById("result")
+const lengthEl = document.getElementById("length")
+const uppercaseEl = document.getElementById("uppercase")
+const lowercaseEl = document.getElementById("lowercase")
+const numbersEl = document.getElementById("numbers")
+const symbolsEl = document.getElementById("symbols")
+const generateEl = document.getElementById("generate")
+const copyEl = document.getElementById("copy")
 
-  for (let i = 0; i < plength; i++) {
-    userPassword += passwordCharSet.charAt(
-      Math.floor(Math.random() * passwordCharSet.length)
-    );
+
+const randomFunc = {
+  upper: getRandomUpper,
+  lower: getRandomlower,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+generateEl.addEventListener('click' , () => {
+  const length = lengthEl.nodeValue;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length,
+  );
+
+})
+
+
+function generatePassword(lower, upper, number, symbol, length){
+
+
+  let generatePassword = '';
+
+  const typesCount = lowewr + upper + number + symbol;
+
+  console.log('typesCount: ', typesCount);
+
+  const typeArr = [{ lower }, {upper}, {number}, {symbol}].fillter
+  (
+    item => Object.valus(item)[0]
+  );
+  console.log('typeArr:', typeArr);
+
+  if(typesCount === 0) {
+    return '';
   }
-  if (userPassword == "") {
-    let alertbox = document.getElementById("alert");
-    alertbox.innerHTML = "Please select an option before generating";
-    alertbox.classList.add("fail");
-    setTimeout(function() {
-      alertbox.classList.remove("fail");
-    }, 3000);
-  } else {
-    passwordFeild.innerHTML = userPassword;
-  }
-  copyButton.setAttribute("data-clipboard-text", userPassword);
+
+  for(let i = 0; i < length; i += typesCount) {
+    typeArr.forEach(type => {
+
+    });
+  })
 }
-generateButton.addEventListener("click", generate);
 
-clipboard.on("success", function(e) {
-  console.info("Action:", e.action);
-  console.info("Text:", e.text);
-  console.info("Trigger:", e.trigger);
-  let alertbox = document.getElementById("alert");
-  alertbox.innerHTML = "Copied!";
-  alertbox.classList.add("success");
-  setTimeout(function() {
-    alertbox.classList.remove("success");
-  }, 3000);
+function getRandomLower() {
+  trturn String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
 
-  e.clearSelection();
-});
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
 
-clipboard.on("error", function(e) {
-  console.error("Action:", e.action);
-  console.error("Trigger:", e.trigger);
-  let alertbox = document.getElementById("alert");
-  alertbox.innerHTML = "Try select the text to copy";
-  alertbox.classList.add("fail");
-  setTimeout(function() {
-    alertbox.classList.remove("fail");
-  }, 3000);
-});
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+function getRandomSymbol() {
+   const symbols = "!@#$%^&*(){}[]+<>/,.';"
+   return symbols[Math.floor(Math.random() * symbols.length))];
+}
+console.log(getRandomNumber());
