cc3b17a5c33753cf24b93cf8d6f5e49f95ca8bde
Chinami Iihama <chinami0420unno@yahoo.co.jp>
2020-01-25 09:49:46 -0800

    revised with TA Erik

--- script.js
+++ script.js
@@ -1,80 +1,92 @@
 //
 
-const resultEl = document.getElementById("result")
-const lengthEl = document.getElementById("length")
-const uppercaseEl = document.getElementById("uppercase")
-const lowercaseEl = document.getElementById("lowercase")
-const numbersEl = document.getElementById("numbers")
-const symbolsEl = document.getElementById("symbols")
-const generateEl = document.getElementById("generate")
-const copyEl = document.getElementById("copy")
-
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("punctuation");
+const generateEl = document.getElementById("generate");
+const copyEl = document.getElementById("copy");
 
 const randomFunc = {
   upper: getRandomUpper,
-  lower: getRandomlower,
+  lower: getRandomLower,
   number: getRandomNumber,
-  symbol: getRandomSymbol,
+  symbol: getRandomSymbol
 };
 
-generateEl.addEventListener('click' , () => {
-  const length = lengthEl.nodeValue;
+generateEl.addEventListener("click", () => {
+  const length = lengthEl.value;
   const hasLower = lowercaseEl.checked;
   const hasUpper = uppercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
 
-  resultEl.innerText = generatePassword(
+  resultEl.value = generatePassword(
     hasLower,
     hasUpper,
     hasNumber,
     hasSymbol,
-    length,
+    length
   );
+});
 
-})
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatePassword = "";
 
+  const typesCount = lower + upper + number + symbol;
 
-function generatePassword(lower, upper, number, symbol, length){
+  console.log("typesCount: ", typesCount);
 
-
-  let generatePassword = '';
-
-  const typesCount = lowewr + upper + number + symbol;
-
-  console.log('typesCount: ', typesCount);
-
-  const typeArr = [{ lower }, {upper}, {number}, {symbol}].fillter
-  (
-    item => Object.valus(item)[0]
+  const typeArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    item => Object.values(item)[0]
   );
-  console.log('typeArr:', typeArr);
+  console.log("typeArr:", typeArr);
 
-  if(typesCount === 0) {
-    return '';
+  if (typesCount === 0) {
+    return "";
   }
 
-  for(let i = 0; i < length; i += typesCount) {
-    typeArr.forEach(type => {
+  var functionArr = [];
+  if (lower) {
+    functionArr.push(getRandomLower);
+  }
+  if (upper) {
+    functionArr.push(getRandomUpper);
+  }
+  if (number) {
+    functionArr.push(getRandomNumber);
+  }
+  if (symbol) {
+    functionArr.push(getRandomSymbol);
+  }
 
-    });
-  })
+  for (let i = 0; i < length; i++) {
+    var x = Math.floor(Math.random() * functionArr.length);
+    generatePassword += functionArr[x]();
+  }
+  console.log(generatePassword);
+  return generatePassword;
 }
 
 function getRandomLower() {
-  trturn String.fromCharCode(Math.floor(Math.random() *26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-   const symbols = "!@#$%^&*(){}[]+<>/,.';"
-   return symbols[Math.floor(Math.random() * symbols.length))];
+  const symbols = "!@#$%^&*(){}[]+<>/,.';";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log(getRandomNumber());
+// console.log(getRandomNumber());
+// console.log(getRandomUpper());
+// console.log(getRandomSymbol());
+// console.log(getRandomLower());
